b641d9d075712f60bc5f351e41a7b33ad08aa310
Diego Cabre <diegocabre@gmail.com>
2023-10-13 19:34:00 -0300

    proyecto terminado con mensaje

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,14 @@
 export default function Landing() {
     const scrollToSection = (sectionId) => {
         const section = document.getElementById(sectionId);
-        if (section) {
+        if (section !== null) {
             section.scrollIntoView({ behavior: "smooth" });
         }
     }
 
     return (
         <div className="landing">
-            <button className="btn" onClick={() => scrollToSection("section")}>2x1 dar click aqui y revisa tus Cocktails</button>
+            <button className="btn" onClick={() => scrollToSection("section")}>2x1 en todos los Cocktails</button>
         </div>
-
     )
 }
